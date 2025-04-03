f9c1ca22f3018c10e8036fb75751991dc78671d1
Adir Gamil <2000adir2000@gmail.com>
2025-04-03 14:23:30 +0300

    add typing animation

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,15 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
 
 const Header = () => {
   return (
     <>
-      <div id='top' className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col justify-center items-center gap-4'>
+      <div
+        id="top"
+        className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col justify-center items-center gap-4"
+      >
         <div>
           <Image
             src={assets.profile_img}
@@ -14,15 +18,32 @@ const Header = () => {
           />
         </div>
         <h3 className="flex items-end text-xl md:text-2xl mb-3 gap-2">
-          Adir Gamil{' '}
+          Hi, My name is <span className="font-bold">Adir Gamil</span>{' '}
           <Image src={assets.hand_icon} alt="profile" className=" w-6" />
         </h3>
-        <h1 className="text-5xl md:text-6xl">Fullstack / Frontend Developer</h1>
-        <p className="max-w-2xl mx-auto">
-          Im a front-end developer with a passion for creating engaging and
-          user-friendly web experiences.
-        </p>
-        <div className='flex flex-col md:flex-row items-center gap-4 mt-4'>
+        <h1 className="text-5xl md:text-6xl h-[120px]">
+          I'm a{' '}
+          <TypeAnimation
+            sequence={[
+              'Full Stack Developer',
+              2000,
+              'React & Next.js Developer',
+              2000,
+              'Frontend Focused',
+              2000,
+              'Turning Design into Code',
+              2000,
+              'TailwindCSS Enthusiast',
+              2000,
+              'Always Learning',
+              2000,
+            ]}
+            wrapper="span"
+            repeat={Infinity}
+          />
+        </h1>
+
+        <div className="flex flex-col md:flex-row items-center gap-4 mt-4">
           <a
             href="#contact"
             className="px-10 hover:scale-110 transition-all duration-200 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
