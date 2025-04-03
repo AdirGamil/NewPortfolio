70b3a14f6caca42a74308ca2967fea2ed2e34054
Adir Gamil <2000adir2000@gmail.com>
2025-04-03 09:41:41 +0300

    update npm

--- app/components/LatestWorks.tsx
+++ app/components/LatestWorks.tsx
@@ -1,16 +1,17 @@
-import Image from "next/image"
+import Image from 'next/image'
+import { FiExternalLink } from 'react-icons/fi'
+
 
 const portfolio = [
   {
     id: 1,
     title: 'Worksapce Landing',
     image:
-      'https://res.cloudinary.com/dhweqnxgd/image/upload/v1743488502/nextjs_qhmbei.png',
+      'https://res.cloudinary.com/drejchdwh/image/upload/v1742561995/8e14459fb3fdde9b3e0eff279161e77d.webp',
     description: 'Description of project 1',
     githubLink: 'https://github.com/username/project1',
     liveLink: 'https://project1.com',
   },
-
 ]
 
 const LatestWorks = () => {
@@ -29,21 +30,32 @@ const LatestWorks = () => {
         {portfolio.map((item) => (
           <div
             key={item.id}
-            className="flex relative flex-col gap-3 items-center justify-center"
+            className="flex relative flex-col gap-3 w-[335px] h-[335px] items-center justify-center"
           >
             <Image
               src={item.image}
               alt={item.title}
-              width={335}
-              height={335}
-              className=" object-cover"
+              fill
+              className="rounded-lg object-cover"
             />
-            <div className="absolute bg-white w-[270px] rounded-lg border border-black flex items-center justify-center">
-              <div>
-                <h1 className="text-lg">{item.title}</h1>
-                <p className="text-sm">{item.description}</p>
+            <div className="absolute bottom-5 left-1/2 -translate-x-1/2 w-[270px] rounded-lg border border-black bg-white px-4 py-3 flex items-center justify-between shadow-md">
+              {/* טקסט */}
+              <div className="flex-1">
+                <h3 className="text-base font-semibold text-gray-800 truncate">
+                  {item.title}
+                </h3>
+                <p className="text-sm text-gray-600 line-clamp-2">
+                  {item.description}
+                </p>
               </div>
-              <button>X</button>
+
+              {/* כפתור חץ */}
+              <button
+                className="w-10 h-10 flex items-center bg-[#D8FC68] justify-center border border-gray-400 rounded-full ml-4 hover:bg-gray-100 transition"
+                aria-label="Open"
+              >
+                  <FiExternalLink className="text-xl" />
+              </button>
             </div>
           </div>
         ))}
